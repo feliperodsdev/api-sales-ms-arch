0afcfe802a24656434783fb5a91067b756178bcd
liber <libertyisrequired@gmail.com>
2023-02-26 11:08:28 -0300

    move files

---
+++ order-service/order-service/src/test/java/com/feliperodsdev/orderservice/OrderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.feliperodsdev.orderservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
